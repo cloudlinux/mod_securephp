81e80dee615a4ee63e397837997b3eb3e9ab4522
root <root@TestApache.(none)>
2014-02-10 05:34:34 +0200

    Added

---
+++ mod_securephp-0.7.2/rebuild.sh
@@ -0,0 +1,10 @@
+make clean
+aclocal
+libtoolize --force
+automake --add-missing
+autoreconf
+./configure --prefix=/usr --sysconfdir=/etc --with-apr=/usr/bin/apr-1-config --with-apxs=/usr/sbin/apxs --with-apache-user=apache --with-setid-mode=paranoid --with-php=/usr/bin/php-cgi --with-logfile=/var/log/httpd/suphp_log --enable-SUPHP_USE_USERGROUP=yes
+# --enable-backwardcompatibility=yes
+make
+# sudo make install
+
