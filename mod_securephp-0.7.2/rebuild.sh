c36bf372e8b8dd6125ddcc2442642d78f66d4395
Maxim Bereznyak <mbereznyak@cloudlinux.com>
2014-02-11 08:47:24 +0200

    Removed sudo

--- mod_securephp-0.7.2/rebuild.sh
+++ mod_securephp-0.7.2/rebuild.sh
@@ -6,5 +6,5 @@ autoreconf
 ./configure --prefix=/usr --sysconfdir=/etc --with-apr=/usr/bin/apr-1-config --with-apxs=/usr/sbin/apxs --with-apache-user=apache --with-setid-mode=paranoid --with-php=/usr/bin/php-cgi --with-logfile=/var/log/httpd/suphp_log --enable-SUPHP_USE_USERGROUP=yes
 # --enable-backwardcompatibility=yes
 make
-# sudo make install
+make install
 
